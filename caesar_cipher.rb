1ebe213e135975c9b70f2b561eb6877d7c368365
N Skutt <nskutt@protonmail.com>
2024-02-29 15:40:25 -0500

    added two tests and got them to pass for the decipher function

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -18,14 +18,14 @@ class CaesarCipher
   p prepare * ""
   end
 
-  def caesar_decipher(input, shift = -3)
-    prepare = input.chars
-    if shift > 25 then shift %= 26 end
+  def caesar_decipher
+    prepare = @input.chars
+    if @shift > 25 then @shift %= 26 end
     prepare.each_index do |letter|
       prepare[letter] = prepare[letter].ord
-      prepare[letter] -= shift unless prepare[letter] < 65 || prepare[letter] > 122
-      if shift > 0 then prepare[letter] += 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] < 65 && prepare[letter] > 39 end
-      if shift < 0 then prepare[letter] -= 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] > 122 end
+      prepare[letter] -= @shift unless prepare[letter] < 65 || prepare[letter] > 122
+      if @shift > 0 then prepare[letter] += 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] < 65 && prepare[letter] > 39 end
+      if @shift < 0 then prepare[letter] -= 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] > 122 end
       prepare[letter] = prepare[letter].chr
     end
     p prepare * ""
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -13,5 +13,14 @@ RSpec.describe CaesarCipher do
       expect(message.caesar_cipher).to eql('defgh')
     end
   end
-
+  describe '#caesar_decipher' do
+    it 'unshifts and returns the input' do
+      message = described_class.new('fghij', 5)
+      expect(message.caesar_decipher).to eql('abcde')
+    end
+    it 'unshifts without a specified number' do
+      message = described_class.new('defgh')
+      expect(message.caesar_decipher).to eql('abcde')
+    end
+  end
 end
