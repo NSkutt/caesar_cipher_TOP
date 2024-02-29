909aad48424fee2b8ee965c910928af7d87db28b
N Skutt <nskutt@protonmail.com>
2024-02-29 17:48:18 -0500

    refactors code to use a spin method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,33 +1,27 @@
 # frozen_string_literal: true
+
 # encrypts and decrypts messages according to the casesar cipher method
 class CaesarCipher
   def initialize(input, shift = 3)
     @input = input
-    @shift = shift
+    @shift = shift % 26
   end
+
   def caesar_cipher
-    prepare = @input.chars
-    if @shift > 25 then @shift %= 26 end
-    prepare.each_index do |letter|
-      prepare[letter] = prepare[letter].ord
-      prepare[letter] += @shift unless prepare[letter] < 65 || prepare[letter] > 122
-      if @shift > 0 then prepare[letter] -= 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] > 122 end
-      if @shift < 0 then prepare[letter] += 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] < 65 && prepare[letter] > 39 end
-      prepare[letter] = prepare[letter].chr
-    end
-  p prepare * ""
+    spin(true)
   end
 
   def caesar_decipher
-    prepare = @input.chars
-    if @shift > 25 then @shift %= 26 end
-    prepare.each_index do |letter|
-      prepare[letter] = prepare[letter].ord
-      prepare[letter] -= @shift unless prepare[letter] < 65 || prepare[letter] > 122
-      if @shift > 0 then prepare[letter] += 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] < 65 && prepare[letter] > 39 end
-      if @shift < 0 then prepare[letter] -= 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] > 122 end
-      prepare[letter] = prepare[letter].chr
+    spin(false)
+  end
+
+  def spin(encode)
+    dials = @input.chars
+    msg = dials.map do |char|
+      tooth = char.ord
+      code = encode ? tooth + @shift : tooth - @shift
+      code.chr
     end
-    p prepare * ""
+    msg * ''
   end
 end
