67bbf5db6096fba96249724fa124afb4bfa05d8e
N Skutt <nskutt@protonmail.com>
2024-02-29 13:33:12 -0500

    first failing test, going to need extensive reworking to fix; creates CaesarCipher class with initialization method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,35 +1,33 @@
-def caesar_cipher(input, shift = 3)
-  prepare = input.chars
-  if shift > 25 then shift %= 26 end
-  prepare.each_index do |letter|
-    prepare[letter] = prepare[letter].ord
-    prepare[letter] += shift unless prepare[letter] < 65 || prepare[letter] > 122
-    if shift > 0 then prepare[letter] -= 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] > 122 end
-    if shift < 0 then prepare[letter] += 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] < 65 && prepare[letter] > 39 end
-    prepare[letter] = prepare[letter].chr
+# frozen_string_literal: true
+# encrypts and decrypts messages according to the casesar cipher method
+class CaesarCipher
+  def initialize(input, shift = 3)
+    @input = input
+    @shift = shift
   end
-p prepare * ""
-end
-
-alpha = caesar_cipher("Alpha", 30)
-alpha
-
-str = caesar_cipher("What a string!", -5)
-str
-
-def caesar_decipher(input, shift = -3)
-  prepare = input.chars
-  if shift > 25 then shift %= 26 end
-  prepare.each_index do |letter|
-    prepare[letter] = prepare[letter].ord
-    prepare[letter] -= shift unless prepare[letter] < 65 || prepare[letter] > 122
-    if shift > 0 then prepare[letter] += 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] < 65 && prepare[letter] > 39 end
-    if shift < 0 then prepare[letter] -= 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] > 122 end
-    prepare[letter] = prepare[letter].chr
-  end
+  def caesar_cipher(input, shift = 3)
+    prepare = input.chars
+    if shift > 25 then shift %= 26 end
+    prepare.each_index do |letter|
+      prepare[letter] = prepare[letter].ord
+      prepare[letter] += shift unless prepare[letter] < 65 || prepare[letter] > 122
+      if shift > 0 then prepare[letter] -= 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] > 122 end
+      if shift < 0 then prepare[letter] += 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] < 65 && prepare[letter] > 39 end
+      prepare[letter] = prepare[letter].chr
+    end
   p prepare * ""
+  end
+
+  def caesar_decipher(input, shift = -3)
+    prepare = input.chars
+    if shift > 25 then shift %= 26 end
+    prepare.each_index do |letter|
+      prepare[letter] = prepare[letter].ord
+      prepare[letter] -= shift unless prepare[letter] < 65 || prepare[letter] > 122
+      if shift > 0 then prepare[letter] += 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] < 65 && prepare[letter] > 39 end
+      if shift < 0 then prepare[letter] -= 26 if prepare[letter] > 90 && prepare[letter] < 97 || prepare[letter] > 122 end
+      prepare[letter] = prepare[letter].chr
+    end
+    p prepare * ""
+  end
 end
-
-caesar_decipher(alpha, 30)
-
-caesar_decipher(str, -5)
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
-describe CaesarCipher do
+require './caesar_cipher.rb'
+
+RSpec.describe CaesarCipher do
+  describe '#caesar_cipher' do
+    it 'shifts and returns the input' do
+      message = described_class.new('abcde', 5)
+      expect (message.caesar_cipher).to eql('fghij')
+    end
+  end
+
 end
