a083729e922bcfd560c1da0cd0328dc28677f42e
N Skutt <nskutt@protonmail.com>
2024-02-29 14:32:54 -0500

    updates gitignore to ignore the .rspec file; two succesful tests on encoding, encoding function now uses instance variables

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -6,7 +6,11 @@ RSpec.describe CaesarCipher do
   describe '#caesar_cipher' do
     it 'shifts and returns the input' do
       message = described_class.new('abcde', 5)
-      expect (message.caesar_cipher).to eql('fghij')
+      expect(message.caesar_cipher).to eql('fghij')
+    end
+    it 'shifts without a specified number' do
+      message = described_class.new('abcde')
+      expect(message.caesar_cipher).to eql('defgh')
     end
   end
 
